a2e5d6430c292dd5756b48c37e1e7eabf84d2b0a
yena choi <geniusyena332@gmail.com>
2022-12-24 19:21:25 +0900

    FEAT: 랜덤 닉네임 제너레이터 util 추가
    Changes to be committed:
    	new file:   src/util/generateRandomName.util.js
    
    RESOLVE: issue #3

---
+++ src/util/generateRandomName.util.js
@@ -0,0 +1,58 @@
+const data = {
+  header: [
+    '매니저',
+    'E반의',
+    '연예인',
+    '과학자',
+    '모범생',
+    '광야의',
+    '밤하늘의',
+    '얼령뚱땅',
+    '천방지축',
+    '재치꾼',
+    '발명꾼',
+    '디버거',
+    '프로개발자',
+    '모함꾼',
+  ],
+  body: [
+    '김수완',
+    '김수정',
+    '김영재',
+    '김재민',
+    '김주향',
+    '김혁찬',
+    '김현우',
+    '김혜란',
+    '김혜주',
+    '노연수',
+    '류제승',
+    '백세현',
+    '변진아',
+    '손유진',
+    '송지수',
+    '이성배',
+    '이성배',
+    '이재정',
+    '이학준',
+    '전상국',
+    '전유진',
+    '최예나',
+    '최혜연',
+    '한요셉',
+    '류현',
+  ],
+};
+
+function getRandomNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function generateRandomName() {
+  const randomHeader = getRandomNum(data.header.length);
+  const randomBody = getRandomNum(data.body.length);
+
+  return data.header[randomHeader] + data.body[randomBody];
+}
+
+console.log(generateRandomName());
