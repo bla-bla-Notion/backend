d869e2a690214024f669f3cb2e56dba93732d5ad
yena choi <geniusyena332@gmail.com>
2022-12-26 01:39:24 +0900

    FEAT: random nickname, disconnected user, message transmission
    Changes to be committed:
    	deleted:    app.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app.js
    	deleted:    src/assets/cart.html
    	deleted:    src/assets/detail.html
    	deleted:    src/assets/favicon.ico
    	deleted:    src/assets/goods.html
    	modified:   src/assets/index.html
    	deleted:    src/assets/logo_big_tr.png
    	deleted:    src/assets/mystyle.css
    	deleted:    src/assets/order.html
    	deleted:    src/assets/register.html
    	new file:   src/public/js/front.js
    	new file:   src/views/home.pug
    	modified:   yarn.lock

--- src/app.js
+++ src/app.js
@@ -7,65 +7,61 @@ const app = express();
 const http = Server(app);
 const io = socketIo(http);
 
-app.use(express.static('assets'));
-app.use(express.json());
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/** 고민거리
+ * case 1
+ socketIdMap의 key 값을 socket.id로 하고, value에 닉네임을 저장하면, 새로운 유저가 접속했을 때, 랜덤으로 생성된 닉네임이 이미 사용 중인지 확인하기 위해, Object.values()로 array를 생성 후, 해당 배열을 for문으로 돌면서 있는지 확인해야 한다.
+ => Array search time complexity = O(n) 동시 접속자 수가 많아지면 엄청난 비효율이 발생한다. 
+ * case2
+ 중복 닉네임 검사 속도를 올리기 위해서는 key 값을 nickname으로 하고, socket.id를 value로 하는 nicknameToSocketIdMap 객체를 하나 더 생성하면 된다. 
+ => Object search time complexity = O(1)  같은 데이터를 하나 더 복제하는 셈으로, 메모리 낭비이다.
+ * 어떤 방법이 좋을지 몰라서 일단 case2로 구현함. 
+ */
 const socketIdMap = {};
+const nicknameToSocketIdMap = {};
 
-function estimateSamePageViewerCount() {
-  const countByUrl = Object.values(socketIdMap).reduce((acc, url) => {
-    return {
-      ...acc,
-      [url]: acc[url] ? acc[url] + 1 : 1,
-    };
-  }, {});
-
-  console.log(countByUrl);
-  for (const [socketId, url] of Object.entries(socketIdMap)) {
-    const count = countByUrl[url];
-    io.to(socketId).emit('SAME_PAGE_VIEWER_COUNT', count);
-  }
-}
-
-// sock 변수에는 접속한 사용자 정보가 담겨 있음
 io.on('connection', sock => {
-  //  프로젝트 요구사항
-  // 입장한 유저별로 랜덤 닉네임을 생성해서 클라로 보내주기
-  // textarea 작업 내용 실시간 렌더링
-  // socket.on("정해야 함!!", (msg) => {})
-  // 퇴장한 유저 닉네임 제거한 유저들 닉네임 배열 클라로 보내주기
-
-  socketIdMap[sock.id] = randomNickNameGenerator();
-  console.log(socketIdMap[sock.id], '유저가 소켓에 연결하였습니다.');
-
-  sock.on('BUY', data => {
-    console.log(data);
-    const emitData = {
-      nickname: data.nickname,
-      goodsId: data.goodsId,
-      goodsName: data.goodsName,
-      date: new Date().toISOString(),
-    };
-
-    io.emit('BUY_GOODS', emitData);
-  });
-
-  sock.on('CHANGED_PAGE', data => {
-    console.log(sock.id, '님이', data, '상세페이지에 접속하셨습니다.');
-    socketIdMap[sock.id] = data;
-    estimateSamePageViewerCount();
+  const nickname = randomNickNameGenerator();
+  while (nicknameToSocketIdMap[nickname]) {
+    nickname = randomNickNameGenerator();
+  }
+  nicknameToSocketIdMap[nickname] = sock.id;
+  socketIdMap[sock.id] = nickname;
+  io.emit('nickname', {
+    newUser: nickname,
+    usersList: Object.values(socketIdMap),
   });
 
   sock.on('disconnect', () => {
-    socketIdMap[sock.id] = null;
-    console.log(sock.id, '유저가 소켓 연결을 끊었습니다.');
+    const disconnectedUser = socketIdMap[sock.id];
+    nicknameToSocketIdMap[socketIdMap[sock.id]] = null;
+    delete socketIdMap[sock.id];
+    io.emit('disconnectedUser', {
+      disconnectedUser,
+      usersList: Object.values(socketIdMap),
+    });
+  });
+
+  sock.on('message', text => {
+    console.log(text);
+    io.emit('message', text);
   });
 });
 
-app.use('/api', require('./routes/index.js'));
-
 http.listen(8080, () => {
-  console.log('서버가 요청을 받을 준비가 됐어요');
+  console.log('8080포트로 서버가 요청을 받을 준비가 됐어요');
 });
+
+//  프로젝트 요구사항
+// 입장한 유저별로 랜덤 닉네임을 생성해서 클라로 보내주기
+// textarea 작업 내용 실시간 렌더링
+// socket.on("정해야 함!!", (msg) => {})
+// 퇴장한 유저 닉네임 제거한 유저들 닉네임 배열 클라로 보내주기
---
+++ src/public/js/front.js
@@ -0,0 +1,29 @@
+const socket = io();
+
+socket.on('nickname', (user, list) => {
+  console.log(user, list);
+});
+
+socket.on('disconnectedUser', (user, list) => {
+  console.log(user, list);
+});
+
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const text = messageForm.querySelector('textarea');
+  socket.emit('message', { text: text.value });
+}
+
+socket.on('message', message => {
+  console.log(message, message.text);
+  const li = document.createElement('li');
+  li.innerText = message.text;
+  const text = messageForm.querySelector('textarea');
+  text.innerText = message.text;
+  messageList.append(li);
+});
+
+messageForm.addEventListener('submit', handleSubmit);
