44a683223e785b95cfecc43dec0ecb43d7920fd2
yena choi <geniusyena332@gmail.com>
2022-12-26 17:53:45 +0900

    FIX : 배포를 위해 테스트용 프론트 페이지 랜더링 미들웨어 주석 처리

--- src/app.js
+++ src/app.js
@@ -16,11 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (_, res) => res.render('home'));
-app.get('/*', (_, res) => res.redirect('/'));
+// app.set('view engine', 'pug');
+// app.set('views', __dirname + '/views');
+// app.use('/public', express.static(__dirname + '/public'));
+// app.get('/', (_, res) => res.render('home'));
+// app.get('/*', (_, res) => res.redirect('/'));
 
 const socketIdMap = {};
 const nicknameToSocketIdMap = {};
