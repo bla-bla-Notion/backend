705b084bd95443bf551486cff8f701318240d1e5
yena choi <geniusyena332@gmail.com>
2022-12-26 17:50:54 +0900

    FEAT: 신규 유저 화면에 페이지 내용 뿌려주기 API 추가
    Changes to be committed:
    	modified:   src/app.js
    
    RESOLVED : issue #13

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const { Server } = require('http');
-const socketIo = require('socket.io');
 const cors = require('cors');
 const randomNickNameGenerator = require('./util/generateRandomName.util');
 
 const app = express();
 const http = Server(app);
-const io = socketIo(http, {
+const io = require('socket.io')(http, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
-    credential: true,
   },
 });
 
@@ -18,27 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).send('bla-bla-Notion 백서버');
-});
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-// app.set('view engine', 'pug');
-// app.set('views', __dirname + '/views');
-// app.use('/public', express.static(__dirname + '/public'));
-// app.get('/', (_, res) => res.render('home'));
-// app.get('/*', (_, res) => res.redirect('/'));
-
-/** 고민거리
- * case 1
- socketIdMap의 key 값을 socket.id로 하고, value에 닉네임을 저장하면, 새로운 유저가 접속했을 때, 랜덤으로 생성된 닉네임이 이미 사용 중인지 확인하기 위해, Object.values()로 array를 생성 후, 해당 배열을 for문으로 돌면서 있는지 확인해야 한다.
- => Array search time complexity = O(n) 동시 접속자 수가 많아지면 엄청난 비효율이 발생한다. 
- * case2
- 중복 닉네임 검사 속도를 올리기 위해서는 key 값을 nickname으로 하고, socket.id를 value로 하는 nicknameToSocketIdMap 객체를 하나 더 생성하면 된다. 
- => Object search time complexity = O(1)  같은 데이터를 하나 더 복제하는 셈으로, 메모리 낭비이다.
- * 어떤 방법이 좋을지 몰라서 일단 case2로 구현함. 
- */
 const socketIdMap = {};
 const nicknameToSocketIdMap = {};
+let document;
 
 io.on('connection', sock => {
   const nickname = randomNickNameGenerator();
@@ -62,8 +48,11 @@ io.on('connection', sock => {
     });
   });
 
+  sock.emit('message', document);
   sock.on('message', text => {
-    io.emit('message', text);
+    sock.broadcast.emit('message', text);
+    sock.emit('message', text);
+    document = text;
   });
 });
 
